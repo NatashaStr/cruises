0488edebb9fa54bbcd52e725313944e5feff58f0
Natalya Sosidka <natapolyakova13@gmail.com>
2023-02-19 21:24:22 +0300

    fixes mobile menu

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+// import {initModals} from './modules/modals/init-modals';
 import {initBurger, breakpointChecker, burgerBtn} from './modules/burger-menu';
 import {initMap} from './modules/map';
 
@@ -7,6 +7,7 @@ import {initMap} from './modules/map';
 
 window.addEventListener('DOMContentLoaded', () => {
 
+  initMap();
 
   if (burgerBtn) {
     initBurger();
@@ -22,8 +23,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
-    initMap();
+    // initModals();
+
   });
 });
 
